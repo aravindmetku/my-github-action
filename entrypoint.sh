ab8c29dfc1fcc7011a3554fc60caea241553fbad
aravindmetku <aravind.metku@leanix.net>
2021-03-08 11:07:27 +0530

    added more functionality to index.js

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -l
 
+npm i
 node index.js
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 console.log("this is printed from node js")
-
+// https://blog.svarun.dev/how-to-create-a-github-action-in-nodejs
 const fs = require('fs');
 const testFolder = './';
 
 fs.readdirSync(testFolder).forEach(file => {
     console.log(file);
 });
+
+const core = require( '@actions/core' );
+const nameToGreet = core.getInput( 'who-to-greet' );
+console.log("name to greet normal value - ", nameToGreet)
+core.setOutput( "time", new Date().toDateString());
