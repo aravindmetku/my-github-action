5ee255f562dfaa8a2c698f1f22693024a1b3983e
aravindmetku <aravind.metku@leanix.net>
2021-03-08 11:19:46 +0530

    checking if input is passed

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 console.log("this is printed from node js")
 // https://blog.svarun.dev/how-to-create-a-github-action-in-nodejs
+// https://github.com/actions/toolkit/tree/main/packages/core
+
 const fs = require('fs');
 const testFolder = './';
 
@@ -8,6 +10,6 @@ fs.readdirSync(testFolder).forEach(file => {
 });
 
 const core = require( '@actions/core' );
-const nameToGreet = core.getInput( 'other-greet' );
+const nameToGreet = core.getInput( 'other-greet' , {required: true});
 console.log("name to greet normal value - ", nameToGreet)
 core.setOutput( "time", new Date().toDateString());
