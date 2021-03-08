3b515a8738b22567d9544d405664d61820cb120a
aravindmetku <aravind.metku@leanix.net>
2021-03-08 11:38:35 +0530

    converting to node action

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-console.log("this is printed from node js")
 // https://blog.svarun.dev/how-to-create-a-github-action-in-nodejs
 // https://github.com/actions/toolkit/tree/main/packages/core
+const core = require('@actions/core');
+
+console.log("this is printed from node js")
 
 const fs = require('fs');
 const testFolder = './';
@@ -9,7 +11,6 @@ fs.readdirSync(testFolder).forEach(file => {
     console.log(file);
 });
 
-const core = require( '@actions/core' );
 const nameToGreet = core.getInput( 'other-greet' , {required: true});
 console.log("name to greet normal value - ", nameToGreet)
 core.setOutput( "time", new Date().toDateString());
