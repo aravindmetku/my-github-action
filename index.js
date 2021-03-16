df9a8a19eded778c0794cc37b10ff3e97eaedcab
aravindmetku <aravind.metku@leanix.net>
2021-03-16 11:09:55 +0530

    check err

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // https://blog.svarun.dev/how-to-create-a-github-action-in-nodejs
 // https://github.com/actions/toolkit/tree/main/packages/core
 const core = require('@actions/core');
-const { exec } = require('child_process');
+const {exec} = require('child_process');
 const fs = require('fs');
 
 exec('npm i license-checker', (err, stdout, stderr) => {
-    if(err) {
+    if (err) {
         console.log('failed to install license checker lib')
     }
 
@@ -14,15 +14,15 @@ exec('npm i license-checker', (err, stdout, stderr) => {
 
     exec('license-checker --json > licenses.json', () => {
         console.log('completed running the command');
+
+        const testFolder = './';
+
+        fs.readdirSync(testFolder).forEach(file => {
+            console.log(file);
+        });
+
+        const nameToGreet = core.getInput('other-greet', {required: true});
+        console.log("name to greet normal value - ", nameToGreet)
+        core.setOutput("time", new Date().toDateString());
     })
 })
-
-const testFolder = './';
-
-fs.readdirSync(testFolder).forEach(file => {
-    console.log(file);
-});
-
-const nameToGreet = core.getInput( 'other-greet' , {required: true});
-console.log("name to greet normal value - ", nameToGreet)
-core.setOutput( "time", new Date().toDateString());
